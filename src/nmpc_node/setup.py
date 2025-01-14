f85eaa35d89a8a35a4710e40cea5b30328580db9
MechaEng1 <dmanganaro2001@gmail.com>
2025-01-14 20:49:22 +0100

    Il file nmpc_node viene buildato e lanciato

--- src/nmpc_node/setup.py
+++ src/nmpc_node/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'nmpc_node = nmpc_node.nmpc_node:main',
+            "nmpc_node = nmpc_node.nmpc_code:main"
         ],
     },
 )
